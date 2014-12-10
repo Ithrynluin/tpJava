925cf04cdf04aa5be0949e4f1bd9f3da83e0ea27
Mardaspan <imbert.kevin94@gmail.com>
2014-12-10 19:14:49 +0100

    ajout javadoc classe ComposantSysteme

--- TP2_IMBERT_JARRIGE_G4B/src/pattern/exo3/composite/ComposantSysteme.java
+++ TP2_IMBERT_JARRIGE_G4B/src/pattern/exo3/composite/ComposantSysteme.java
@@ -2,28 +2,64 @@ package pattern.exo3.composite;
 
 import pattern.exo3.visiteur.Visiteur;
 
+/**
+ * Classe abstraite représentant un composant du système
+ * @author Imbert / Jarrige
+ *
+ */
 public abstract class ComposantSysteme {
 	
+	/**
+	 * Retourne le nom du composant
+	 * @return nom du composant
+	 */
 	public String getNom(){
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * Retourne la taille du composant
+	 * @return taille du composant
+	 */
 	public int getTaille(){
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * Méthode permettant d'ajouter un composants dans un autre composant
+	 * (ici il s'agira surtout de mettre des fichiers dans un répertoire)
+	 * @param composant
+	 * 				le composant à ajouter
+	 */
 	public void addComposantSysteme(ComposantSysteme composant){
 		throw new UnsupportedOperationException();
 	}
 	
-	public void removecomposantSysteme(ComposantSysteme composant){
+	/**
+	 * Méthode permettant d'enlever un composant d'un autre composant
+	 * (ici il s'agira surtout de supprimer un fichier d'un répertoire)
+	 * @param composant
+	 * 				le composant à enlever
+	 */
+	public void removeComposantSysteme(ComposantSysteme composant){
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * Méthode renvoyant le composant à un index donné d'une liste de composant
+	 * @param index
+	 * 			le numéro où se situe le composant dans la liste
+	 * @return le composant situé à l'adresse indiqué en paramètre
+	 */
 	public ComposantSysteme getComposantSysteme(int index){
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * Méthode permettant à un composant d'accepter un visiteur
+	 * @param visiteur
+	 * 				le visiteur accepté par le composant
+	 */
 	public void acceptVisiteur(Visiteur visiteur){
 		throw new UnsupportedOperationException();
 	}
